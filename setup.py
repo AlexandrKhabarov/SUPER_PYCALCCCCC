4b471fc3160e7e236286c90174799c518a7528ee
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2018-10-20 19:06:19 +0300

    added setup file for publishing package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'SUPER_PYCALCCCCC',
+    version = '0.0.1',
+    description = 'PYCALC',
+    author="Alexander Khabarov",
+    author_email="rob.sumsung.mi3@gmail.com",
+    packages = find_packages(),
+    include_package_data=True,
+    url="https://github.com/AlexandrKhabarov/SUPER_PYCALCCCCC",
+)
+
