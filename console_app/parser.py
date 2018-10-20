c7361913be80f0497ca9404d06b4ab9e38342b9f
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2018-10-20 22:52:09 +0300

    added support of many plugable modules

--- console_app/parser.py
+++ console_app/parser.py
@@ -5,9 +5,9 @@ class ArgumentParser:
 
     @classmethod
     def parse_arguments(cls):
-        parser = argparse.ArgumentParser(description='Process some integers.')
+        parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
         parser.add_argument('math_expr', type=str, help='mathematic expression')
-        parser.add_argument("-m", '--module', type=str, help="module_name")
+        parser.add_argument("-m", '--module', nargs="+", default=[], help="module_name")
 
         args = parser.parse_args()
         return args.math_expr, args.module
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from core.recognizer import calculation
 from core.utils import add_available_names_from_module
 
 if __name__ == "__main__":
-    math_expr, module_name = ArgumentParser.parse_arguments()
-    if module_name:
+    math_expr, module_names = ArgumentParser.parse_arguments()
+    for module_name in module_names:
         add_available_names_from_module(available_functions, available_constants, module_name)
     print(calculation(math_expr))
