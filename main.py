07438c80bb4638558a13e9c6528d582409b22670
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2018-10-20 20:56:43 +0300

    added checking for input module parameter

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from core.operatios import available_functions, available_constants
 
 if __name__ == "__main__":
     math_expr, module = ArgumentParser.parse_arguments()
-    module = __import__(module)
-    available_functions_names_of_module = filter(lambda x: not x.startswith("_"), dir(module))
-    for avai_name in available_functions_names_of_module:
-        obj = getattr(module, avai_name)
-        if inspect.isfunction(obj) or inspect.isbuiltin(obj):
-            available_functions.update({avai_name: obj})
-        if isinstance(obj, int) or isinstance(obj, float):
-            available_constants.update({avai_name: obj})
+    if module:
+        module = __import__(module)
+        available_functions_names_of_module = filter(lambda x: not x.startswith("_"), dir(module))
+        for avai_name in available_functions_names_of_module:
+            obj = getattr(module, avai_name)
+            if inspect.isfunction(obj) or inspect.isbuiltin(obj):
+                available_functions.update({avai_name: obj})
+            if isinstance(obj, int) or isinstance(obj, float):
+                available_constants.update({avai_name: obj})
     print(calculation(math_expr))
