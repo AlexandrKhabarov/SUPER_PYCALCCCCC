34e704548eb30f1e47f47296e58ba67a0cc09007
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2018-10-21 00:46:07 +0300

    added correct output of error

--- pycalc/main.py
+++ pycalc/main.py
@@ -8,7 +8,10 @@ def main():
     math_expr, module_names = ArgumentParser.parse_arguments()
     for module_name in module_names:
         add_available_names_from_module(available_functions, available_constants, module_name)
-    print(calculation(math_expr))
+    try:
+        print(calculation(math_expr))
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
