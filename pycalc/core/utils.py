23d2326af8a968698a0eb00d9292bb919575c994
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2018-10-23 20:06:27 +0300

    refactor function for adding modules for calculation

--- pycalc/core/utils.py
+++ pycalc/core/utils.py
@@ -9,9 +9,23 @@ def add_available_names_from_module(available_functions, available_constants, mo
         available_functions_names_of_module = filter(lambda x: not x.startswith("_"), dir(module))
         for available_name in available_functions_names_of_module:
             obj = getattr(module, available_name)
-            if callable(obj) or inspect.isbuiltin(obj):
-                available_functions.update({available_name: obj})
-            if isinstance(obj, int) or isinstance(obj, float):
-                available_constants.update({available_name: obj})
+            if _is_func(obj):
+                _add_to_the_namespace(available_functions, available_name, obj, prefix="Function")
+            elif _is_const(obj):
+                _add_to_the_namespace(available_constants, available_name, obj, prefix="Const")
     except ModuleNotFoundError:
         raise ModuleNotFound()
+
+
+def _is_const(obj):
+    return isinstance(obj, int) or isinstance(obj, float)
+
+
+def _is_func(obj):
+    return callable(obj) or inspect.isbuiltin(obj)
+
+
+def _add_to_the_namespace(namespace, available_name, obj, silent=True, prefix='Obj'):
+    if available_name in namespace and not silent:
+        print(f"{prefix} with name: {available_name} already exist in namespace.")
+    namespace.update({available_name: obj})
