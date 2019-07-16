3f9545ff2fb490248f1dacb947fbe078806dfce6
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2019-07-16 20:00:58 +0300

    delete legacy

--- pycalc/core/available_names.py
+++
@@ -1,7 +0,0 @@
-from pycalc.core.utils import add_available_names_from_module
-
-available_functions = {}
-available_constants = {}
-
-add_available_names_from_module(available_functions, available_constants, "math")
-add_available_names_from_module(available_functions, available_constants, "builtins")
