d59a192ca19b620058e92adafedd1468e6b1e49f
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2020-04-21 12:26:29 +0300

    added test_suite discover in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
-from setuptools import setup
+import os
+import unittest
+
+from setuptools import setup, find_packages
+
+root_dir = os.path.dirname(__file__)
+
+
+def pycalc_test_suite():
+    test_loader = unittest.TestLoader()
+    test_suite = test_loader.discover(os.path.join(root_dir, 'tests'), pattern='test_*.py')
+    return test_suite
+
 
 setup(
-    name='super_puper_pycalc',
+    name='pycalc',
     version='0.0.1',
     description="Pure-python command-line calculator.",
+    license='Apache License 2.0',
     author="Alexander Khabarov",
     author_email="rob.sumsung.mi3@gmail.com",
-    packages=["pycalc", "pycalc.core"],
-    test_suite="pycalc.tests",
-    url="https://github.com/AlexandrKhabarov/SUPER_PYCALCCCCC",
+    packages=find_packages(exclude=['tests*']),
+    test_suite='setup.pycalc_test_suite',
+    url="https://github.com/AlexandrKhabarov/pycalc",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
@@ -18,5 +31,6 @@ setup(
         'console_scripts': [
             'pycalc = pycalc.main:main',
         ]
-    }
+    },
+    python_requires='~=3.6',
 )
