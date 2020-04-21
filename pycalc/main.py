a80aa966cf0217e535552674e3e35064297a8c6c
AlexandrKhabarov <rob.sumsung.mi3@gmail.com>
2020-04-21 11:53:30 +0300

    hide imports and definition of command line interface in main function

--- pycalc/main.py
+++ pycalc/main.py
@@ -1,19 +1,17 @@
-import argparse
-
-from pycalc.core.calculator import calculate
-
-
-def parse_arguments():
-    parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
-
-    parser.add_argument('expression', type=str, help='Mathematics expression')
-    parser.add_argument("-m", '--module', nargs="+", default=[], help="module_name")
-
-    args = parser.parse_args()
-    return args.expression, args.module
-
-
 def main():
+    import argparse
+
+    from pycalc.core.calculator import calculate
+
+    def parse_arguments():
+        parser = argparse.ArgumentParser(description='Pure-python command-line calculator.')
+
+        parser.add_argument('expression', type=str, help='Mathematics expression')
+        parser.add_argument("-m", '--module', nargs="+", default=[], help="module_name")
+
+        args = parser.parse_args()
+        return args.expression, args.module
+
     expression, module_names = parse_arguments()
     result = calculate(expression, module_names)
     return result
